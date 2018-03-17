e118e280e0099f3c1f1b5465939109056b3f8f17
Ryan King <kinggryan@gmail.com>
2018-03-17 11:11:15 -0700

    Camera improvements

--- Assets/Scripts/Controllers/DriverCameraController.cs
+++ Assets/Scripts/Controllers/DriverCameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DriverCameraController : MonoBehaviour {
 
     public Rigidbody car;
+    public SimpleCarController carController;
 
     float followDistance = 10f;
     float followHeight = 4f;
@@ -22,50 +23,38 @@ public class DriverCameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        // Determine where to move to
-        // var carDirection = Vector3.ProjectOnPlane(car.velocity, Vector3.up).normalized;
-        // var carFacingDirection = Vector3.ProjectOnPlane(car.transform.forward, Vector3.up).normalized;
-        // var angleDifference = Vector3.SignedAngle(carFacingDirection, Vector3.ProjectOnPlane(transform.forward, Vector3.up), Vector3.up);
-        // Debug.Log(angleDifference);
-        // if (Mathf.Abs(angleDifference) > maxDifferenceBetweenForwardAndCamera)
-        // {
-        //     carDirection = Quaternion.AngleAxis(maxDifferenceBetweenForwardAndCamera * -Mathf.Sign(angleDifference), Vector3.up) * carFacingDirection;
-        //     //carDirection = Vector3.RotateTowards(carDirection, carFacingDirection, Mathf.Deg2Rad*angleDifference, Mathf.Infinity);
-        // }
-        //// carDirection = car.transform.InverseTransformDirection(carDirection);
-        // var targetPositionRelativeToCar =  -followDistance * carDirection + followHeight * transform.up;
-        // var currentPositionRelativeToCar = transform.position - car.position;
-        // var newPositionRelativeToCar = Vector3.RotateTowards(currentPositionRelativeToCar, targetPositionRelativeToCar, Mathf.Infinity, Mathf.Infinity);
-        // transform.position = car.position + newPositionRelativeToCar;
+        if (carController.IsGrounded())
+            PositionCameraForGroundedCar();
+        else
+            PositionCameraForAirborneCar();
+    }
+    
+    void PositionCameraForGroundedCar()
+    {
+        //var targetPositionRelativeToCar = -followDistance * car.transform.forward + followHeight * transform.up;
+        //var currentPositionRelativeToCar = transform.position - car.position;
+        //var newPositionRelativeToCar = Vector3.RotateTowards(currentPositionRelativeToCar, targetPositionRelativeToCar, Mathf.Infinity, Mathf.Infinity);
+        //transform.position = car.position + newPositionRelativeToCar;
+        transform.position = car.position - followDistance * Vector3.ProjectOnPlane(car.transform.forward, Vector3.up).normalized + followHeight * Vector3.up;
 
-        var targetPositionRelativeToCar = -followDistance * car.transform.forward + followHeight * transform.up;
-        var currentPositionRelativeToCar = transform.position - car.position;
-        var newPositionRelativeToCar = Vector3.RotateTowards(currentPositionRelativeToCar, targetPositionRelativeToCar, Mathf.Infinity, Mathf.Infinity);
-        transform.position = car.position + newPositionRelativeToCar;
-
-        // There are two target locations we want to get to
-        // If either of them is too far away, we should go to that one
-        // Try to hit the position based on velocity
-        // But pull it towards the forward facing direction if they mismatch by a ton
-
-        //var targetPositionProjection = Vector3.ProjectOnPlane(targetPosition, Vector3.up);
-        //var currentPositionProjection = Vector3.ProjectOnPlane(transform.position, Vector3.up);
-        //var rotationAngle = Vector3.Angle(currentPositionProjection, targetPositionProjection);
-        //Debug.Log("angle " + rotationAngle);
-        //transform.RotateAround(car.position, Vector3.up, rotationAngle);
-
-        // Move there
-        //  var newPositionCarSpace = Vector3.RotateTowards(currentPositionCarSpace, targetPositionCarSpace, Mathf.Infinity, lerpRate * Time.deltaTime);
-        //   transform.position = car.transform.TransformPoint(newPositionCarSpace);
-
-        // Look at the target looky position
-        // var targetLookPosition = car.position + lookAheadDistance * carDirection;
-        var lookPosition = car.position + lookAheadDistance * car.transform.forward; // Vector3.Lerp(previousLookPosition, targetLookPosition, lerpRate * Time.deltaTime);
-        transform.LookAt(lookPosition);
-     //   previousLookPosition = lookPosition;
+        SetLookDirection();
     }
 
-    
+    void PositionCameraForAirborneCar()
+    {
+        var groundDirectionFromCarToPlayer = Vector3.ProjectOnPlane(transform.position - car.position, Vector3.up).normalized;
+        transform.position = car.position + followDistance * groundDirectionFromCarToPlayer + followHeight * Vector3.up;
+
+        SetLookDirection();
+    }
+
+    void SetLookDirection()
+    {
+        var cameraToCarGroundDirection = Vector3.ProjectOnPlane(car.position - transform.position, Vector3.up).normalized;
+        var cameraToCarGroundDistance = Vector3.ProjectOnPlane(car.position - transform.position, Vector3.up).magnitude;
+        var lookAtPoint = transform.position - followHeight * Vector3.up + (cameraToCarGroundDistance + lookAheadDistance) * cameraToCarGroundDirection;
+        transform.LookAt(lookAtPoint);
+    }
 
     float FollowDistanceFromCarSpeed(float speed)
     {
