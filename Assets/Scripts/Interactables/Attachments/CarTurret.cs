a4f14d6c0c1c0611bf3ad359566cd6c4340c5a76
Ryan King <kinggryan@gmail.com>
2018-11-01 11:59:17 -0700

    Turret costs scrap

--- Assets/Scripts/Interactables/Attachments/CarTurret.cs
+++ Assets/Scripts/Interactables/Attachments/CarTurret.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarTurret : CarAttachment {
-
-    public Rigidbody car;
+    
     public Collider carBodyCollider;
     public GameObject bulletPrefab;
     public float fireRate;
     public float bulletSpeed;
+    public float scrapCostPerBullet = 1f;
 
     private float fireCooldown;
 
@@ -16,12 +16,12 @@ public class CarTurret : CarAttachment {
 	void Update () {
         fireCooldown = Mathf.Max(0, fireCooldown - Time.deltaTime);
         // Debug.Log("B: " + beingUsed + "F: " + fireCooldown);
-		if(beingUsed && fireCooldown <= 0)
+		if(beingUsed && fireCooldown <= 0 && car.PayScrap(scrapCostPerBullet))
         {
             fireCooldown += 1 / fireRate;
             var bulletObject = GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             var bulletRbody = bulletObject.GetComponent<Rigidbody>();
-            bulletRbody.velocity = car.velocity + bulletSpeed * transform.forward;
+            bulletRbody.velocity = car.rbody.velocity + bulletSpeed * transform.forward;
 
             // Cannot shoot self
             // At some point, the turret should also restrict the look angles of the player so they can't for instance look straight down and fire
